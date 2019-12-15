3a95f96a7fdd99cca97e5a26ffae47394b411444
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2019-12-15 23:15:25 +0100

    0.0.19

--- src/main/java/zeroneye/lib/client/util/Draw3d.java
+++ src/main/java/zeroneye/lib/client/util/Draw3d.java
@@ -1,46 +1,9 @@
 package zeroneye.lib.client.util;
 
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.Direction;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import org.lwjgl.opengl.GL11;
 
 @OnlyIn(Dist.CLIENT)
 public class Draw3d {
-    public static void cube(BufferBuilder bb) {
-        for (Direction facing : Direction.values()) {
-            quad(bb, facing, 1.0D, 1.0D);
-        }
-    }
 
-    public static void quad(BufferBuilder bb, Direction facing, double deep, double scale) {
-        double[][] pos = new double[4][3];
-        double[][] uv = new double[4][2];
-        switch (facing) { // TODO finish all facing quads
-            case DOWN:
-                break;
-            case UP:
-                pos[0][0] = pos[3][0] = pos[2][2] = pos[3][2] = -scale * 0.5D;
-                pos[0][1] = pos[1][1] = pos[2][1] = pos[3][1] = deep;
-                pos[1][0] = pos[2][0] = pos[0][2] = pos[1][2] = scale * 0.5D;
-                uv[0][0] = uv[0][1] = uv[1][0] = uv[3][1];
-                uv[1][1] = uv[2][0] = uv[2][1] = uv[3][0] = 1.0D;
-                break;
-            case NORTH:
-            case SOUTH:
-            case WEST:
-            case EAST:
-        }
-        quad(bb, pos, uv);
-    }
-
-    public static void quad(BufferBuilder bb, double[][] pos, double[][] uv) {
-        bb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-        bb.pos(pos[0][0], pos[0][1], pos[0][2]).tex(uv[0][0], uv[0][1]).endVertex();
-        bb.pos(pos[1][0], pos[1][1], pos[1][2]).tex(uv[1][0], uv[1][1]).endVertex();
-        bb.pos(pos[2][0], pos[2][1], pos[2][2]).tex(uv[2][0], uv[2][1]).endVertex();
-        bb.pos(pos[3][0], pos[3][1], pos[3][2]).tex(uv[3][0], uv[3][1]).endVertex();
-    }
 }
--- src/main/java/zeroneye/lib/util/Energy.java
+++ src/main/java/zeroneye/lib/util/Energy.java
@@ -3,8 +3,6 @@ package zeroneye.lib.util;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
@@ -36,11 +34,6 @@ public class Energy {
         return i[0];
     }
 
-    public static int receive(World world, BlockPos pos, Direction direction, int energy, boolean simulate) {
-        TileEntity tile = world.getTileEntity(pos);
-        return receive(tile, direction, energy, simulate);
-    }
-
     public static int receive(@Nullable TileEntity tile, Direction direction, int energy, boolean simulate) {
         final int[] i = {0};
         if (tile != null) {
@@ -49,11 +42,6 @@ public class Energy {
             });
         }
         return i[0];
-    }
-
-    public static int extract(World world, BlockPos pos, Direction direction, int energy, boolean simulate) {
-        TileEntity tile = world.getTileEntity(pos);
-        return extract(tile, direction, energy, simulate);
     }
 
     public static int extract(@Nullable TileEntity tile, Direction direction, int energy, boolean simulate) {
@@ -66,7 +54,7 @@ public class Energy {
         return i[0];
     }
 
-    public static boolean hasEnergy(ItemStack stack) {
+    public static boolean isPresent(ItemStack stack) {
         return getForgeEnergy(stack).isPresent();
     }
 
@@ -74,16 +62,48 @@ public class Energy {
         return stack.getCapability(CapabilityEnergy.ENERGY, null);
     }
 
-    public static boolean hasEnergy(World world, BlockPos pos, @Nullable Direction direction) {
-        return getForgeEnergy(world, pos, direction).isPresent();
-    }
-
-    public static boolean hasEnergy(@Nullable TileEntity tile, @Nullable Direction direction) {
+    public static boolean isPresent(@Nullable TileEntity tile, @Nullable Direction direction) {
         return getForgeEnergy(tile, direction).isPresent();
     }
 
-    public static LazyOptional<IEnergyStorage> getForgeEnergy(World world, BlockPos pos, @Nullable Direction direction) {
-        return getForgeEnergy(world.getTileEntity(pos), direction);
+    public static boolean canExtract(@Nullable TileEntity tile, @Nullable Direction direction) {
+        final boolean[] flag = {false};
+        if (tile != null) {
+            getForgeEnergy(tile, direction).ifPresent(storage -> {
+                flag[0] = storage.canExtract() && storage.extractEnergy(Integer.MAX_VALUE, true) > 0;
+            });
+        }
+        return flag[0];
+    }
+
+    public static boolean canReceive(@Nullable TileEntity tile, @Nullable Direction direction) {
+        final boolean[] flag = {false};
+        if (tile != null) {
+            getForgeEnergy(tile, direction).ifPresent(storage -> {
+                flag[0] = storage.canReceive() && storage.receiveEnergy(Integer.MAX_VALUE, true) > 0;
+            });
+        }
+        return flag[0];
+    }
+
+    public static int maxExtract(@Nullable TileEntity tile, @Nullable Direction direction) {
+        final int[] i = {0};
+        if (tile != null) {
+            getForgeEnergy(tile, direction).ifPresent(storage -> {
+                i[0] = storage.extractEnergy(Integer.MAX_VALUE, true);
+            });
+        }
+        return i[0];
+    }
+
+    public static int maxReceive(@Nullable TileEntity tile, @Nullable Direction direction) {
+        final int[] i = {0};
+        if (tile != null) {
+            getForgeEnergy(tile, direction).ifPresent(storage -> {
+                i[0] = storage.receiveEnergy(Integer.MAX_VALUE, true);
+            });
+        }
+        return i[0];
     }
 
     public static LazyOptional<IEnergyStorage> getForgeEnergy(@Nullable TileEntity tile, @Nullable Direction direction) {
