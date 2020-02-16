f75f15decfad4d4750391aba326b0ca2bebf3177
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2020-02-16 13:29:41 +0100

    1.15 Port.. f4.

--- src/main/java/owmii/lib/util/FML.java
+++ src/main/java/owmii/lib/util/FML.java
@@ -1,9 +1,18 @@
 package owmii.lib.util;
 
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 public class FML {
-    public static String activeID() {
+    public static String getActiveID() {
         return ModLoadingContext.get().getActiveNamespace();
+    }
+
+    public static boolean isServer() {
+        return !isClient();
+    }
+
+    public static boolean isClient() {
+        return FMLEnvironment.dist.isClient();
     }
 }
